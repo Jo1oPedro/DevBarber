dac0f791cc1a3e305321d4aa67d7821cf989ea00
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-03-14 04:52:25 -0300

    Adicionando as rotas para api

--- ApiDevBarber/routes/api.php
+++ ApiDevBarber/routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BarberController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +21,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/ping', function() {
     return ['pong' => true];
 });
+
+Route::post('/auth/login', [AuthController::class, 'login'])->name('login');
+Route::post('auth/logout', [AuthController::class, 'logout'])->name('logout');
+Route::post('auth/refresh', [AuthController::class, 'refresh'])->name('refresh');
+Route::post('/user', [AuthController::class, 'create'])->name('createUser');
+
+Route::get('/user', [UserController::class, 'read'])->name('getUser');
+Route::put('/user', [UserController::class, 'update'])->name('updateUser');
+Route::get('/user/favorites', [UserController::class, 'getFavorites'])->name('getFavoriteBarber');
+Route::post('/user/favorite', [UserController::class, 'addFavorite'])->name('addFavoriteBarber');
+Route::get('/user/appointments', [UserController::class, 'getAppointments'])->name('getAppointments');
+
+Route::get('/barbers', [BarberController::class, 'list'])->name('listBarbers');
+Route::get('/barber/{id}', [BarberController::class, 'one'])->name('getBarber');
+Route::post('/barber/{id}/appointment', [BarberController::class, 'setAppointment'])->name('setAppointment');
+
+Route::get('/search', [BarberController::class, 'search'])->name('searchBarber');
 
 /*Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
