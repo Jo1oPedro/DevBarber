d29fad3fadb1aad6d2e3bb5373291c83cd09854a
João Pedro <joao.pedreira@estudante.ufjf.br>
2022-03-19 04:03:48 -0300

    Adicionando metodo create ao authController

--- ApiDevBarber/app/Http/Controllers/AuthController.php
+++ ApiDevBarber/app/Http/Controllers/AuthController.php
@@ -3,8 +3,40 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    //
+    public function __construct() {
+        $this->middleware('auth:api', ['except' => ['create', 'login']]);
+    }
+
+    public function create(Request $request) {
+        $array = ['error' => ''];
+
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        if($validator->fails()) {
+            $array['error'] = 'Dados incorretos';
+            return $array;
+        }
+
+        $name = $request->name;
+        $email = $request->email;
+        $password = $request->password;
+        $hash = password_hash($password, PASSWORD_DEFAULT);
+
+        $newUser = new User();
+        $newUser->name = $name;
+        $newUser->email = $email;
+        $newUser->password = $hash;
+        $newUser->save();
+        return $array;
+    }
 }
